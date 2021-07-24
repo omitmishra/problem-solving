e6ed837b5980c9352b788bfb58bceac96cb60870
omitmishra <49590835+omitmishra@users.noreply.github.com>
2021-07-24 17:03:06 +0530

    Time: 40 ms (53.99%), Space: 39.2 MB (74.73%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.java
+++ partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,39 +1,32 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-       if (nums.length == 0) {
-            return true;
-        }
-        
-        int n = nums.length;
-        int sum = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-        }
-        
-        if (sum % 2 == 1) {
-            return false;
-        } 
-        
-        sum = sum / 2;
-        
-        boolean[][] t  = new boolean[n+1][sum+1];
-        
-        for (int i = 0; i <= n; i++) {
-            t[i][0] = true;
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (nums[i-1] > j) {
-                    t[i][j] = t[i-1][j]; 
-                 } else {
-                    t[i][j] = t[i-1][j] || t[i-1][j - nums[i-1]];
+              int sum = 0;
+        for (int number : nums)
+            sum += number;
+        if (sum % 2 != 0)
+            return false; // this is the first condition to check
+        // now the main thing to note is that if we can make a subset with subsetSum = sum / 2, then surely the other
+        // subset can also make the subsetSum = sum / 2;
+        // ie we make 2 subsets s1, s2. If we make s1 = totalSum / 2, then it is sure that the next subset s2 will make
+        // subsetSum = totalSum / 2, so we can return true.
+    
+        int neededSum = sum / 2;
+        // now we form a bool dp array
+        boolean [][] dp = new boolean[nums.length + 1][neededSum+1];
+        // subset sum problem
+        for (int i=0; i<=nums.length; i++) {
+            for (int j=0; j<=neededSum; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = false;
+                else if (nums[i-1] > j) {
+                    dp[i][j] = dp[i-1][j];
                 }
+                else if (nums[i-1] == j)
+                    dp[i][j] = true;
+                else
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i-1]];
             }
         }
-        
-        
-        return t[n][sum];
-    } 
+        return dp[nums.length][neededSum];
+    }
 }
