6a65091e1bcbd1389e4451d0f157cea5b48e8525
omitmishra <49590835+omitmishra@users.noreply.github.com>
2021-07-25 13:30:48 +0530

    Time: 18 ms (76.59%), Space: 44.1 MB (69.20%) - LeetHub

---
+++ find-duplicate-subtrees/find-duplicate-subtrees.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        List<TreeNode> list=new ArrayList<>();
+        HashMap<String,Integer> hm=new HashMap<>();
+        helper(root,hm,list);
+        return list;
+    }
+    public String helper(TreeNode root,HashMap<String,Integer> hm,List<TreeNode> list){
+        if(root==null){
+            return "X";
+        }
+        String left=helper(root.left,hm,list);
+        String right=helper(root.right,hm,list);
+        String temp=root.val+" "+left+right;
+        hm.put(temp,hm.getOrDefault(temp,0)+1);
+        if(hm.get(temp)==2){
+            list.add(root);
+        }
+        return temp;
+    } 
+
+}
