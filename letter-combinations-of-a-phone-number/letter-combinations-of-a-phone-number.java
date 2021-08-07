d8cbb963a1080410667c7cb6058821ebb2bacffa
omitmishra <49590835+omitmishra@users.noreply.github.com>
2021-08-07 15:28:20 +0530

    Time: 1 ms (70.84%), Space: 39.1 MB (40.28%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,5 +1,5 @@
 class Solution {
-  Map<String, String> phone = new HashMap<String, String>() {{
+  Map<String,String> map=new HashMap<>(){{
     put("2", "abc");
     put("3", "def");
     put("4", "ghi");
@@ -8,34 +8,30 @@ class Solution {
     put("7", "pqrs");
     put("8", "tuv");
     put("9", "wxyz");
-  }};
-
-  List<String> output = new ArrayList<String>();
-
-  public void backtrack(String combination, String next_digits) {
-    // if there is no more digits to check
-    if (next_digits.length() == 0) {
-      // the combination is done
-      output.add(combination);
+    }};
+     
+    
+    List<String> list =new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() != 0)
+        get(digits,"");
+        
+        return list;
     }
-    // if there are still digits to check
-    else {
-      // iterate over all letters which map 
-      // the next available digit
-      String digit = next_digits.substring(0, 1);
-      String letters = phone.get(digit);
-      for (int i = 0; i < letters.length(); i++) {
-        String letter = phone.get(digit).substring(i, i + 1);
-        // append the current letter to the combination
-        // and proceed to the next digits
-        backtrack(combination + letter, next_digits.substring(1));
-      }
-    }
-  }
-
-  public List<String> letterCombinations(String digits) {
-    if (digits.length() != 0)
-      backtrack("", digits);
-    return output;
-  }
+    
+   void  get(String str,String ans){
+       if(str.length()==0){
+           list.add(ans);   
+           return ;
+       }
+         String temp=str.substring(0,1);
+         String temp1= map.get(temp);
+       for(int i=0;i<temp1.length();i++){
+           //char ch=temp1.charAt(st);
+            String letter =temp1.substring(i, i + 1);
+           
+           get(str.substring(1),ans+letter);
+       
+         }
+   }
 }
