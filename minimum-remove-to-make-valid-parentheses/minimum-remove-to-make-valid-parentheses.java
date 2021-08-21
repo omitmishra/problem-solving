8c63dfef6e5596ebb3f661366bd7a6800aeae3d5
Ravi <49590835+omitmishra@users.noreply.github.com>
2021-08-21 11:22:43 +0530

    Time: 11 ms (92.01%), Space: 39.8 MB (56.69%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,25 +1,31 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        StringBuilder sb = new StringBuilder();
-        int open = 0;
-    for (char c : s.toCharArray()) {
-        if (c == '(') {
-            open++;
-        } else if (c == ')') {
-            if (open == 0) 
-                continue;
-            open--;
+        StringBuilder sb=new StringBuilder();
+        int open=0;
+        for(char c:s.toCharArray()){
+            char temp =c;
+            if(temp=='('){
+                open++;
+            }else if(temp==')'){
+                if (open==0)
+                    continue;
+                open--;
+            }
+            
+            sb.append(temp);
+            
+            
         }
-        sb.append(c);
-    }
-    
-    for (int i = sb.length() - 1; i >= 0; i--) {
-        if (sb.charAt(i) == '('  && open > 0) {
-            sb.deleteCharAt(i);
-            open--;
+        
+        for(int i=sb.length()-1;i>=0;i--){
+            
+            if(sb.charAt(i)=='(' && open>0){
+                sb.deleteCharAt(i);
+                open--;
+            }
         }
-    }
-    
-    return sb.toString();
+        
+        return sb.toString();
+        
     }
 }
