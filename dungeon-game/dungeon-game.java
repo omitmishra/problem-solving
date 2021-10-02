8570dcfa8114ff5df8036bf989890716924229b1
Ravi <49590835+omitmishra@users.noreply.github.com>
2021-10-02 08:35:50 +0530

    Time: 1 ms (91.00%), Space: 38.2 MB (96.58%) - LeetHub

--- dungeon-game/dungeon-game.java
+++ dungeon-game/dungeon-game.java
@@ -5,8 +5,7 @@ class Solution {
         int [][] dp = new int[row+1][col+1];
         for (int i=0; i<=row; i++)
             Arrays.fill(dp[i], Integer.MAX_VALUE);
-        dp[row-1][col] = 1;
-       // dp[row][col-1] = 1;
+         dp[row-1][col] = 1;
         for (int i=row-1; i>=0; i--) {
             for (int j=col-1; j>=0; j--) {
                 int minimumHealth = Math.min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j];
