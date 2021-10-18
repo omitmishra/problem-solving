3b4d1925af5a441a5f83cc9fac8727a814595d52
Ravi <49590835+omitmishra@users.noreply.github.com>
2021-10-18 06:57:31 +0530

    Time: 0 ms (100.00%), Space: 36.9 MB (49.16%) - LeetHub

---
+++ cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+       if(root.val == x || root.val == y) return false;
+        
+        
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        boolean xFound = false;
+        boolean yFound = false;
+        
+        int parentX = 0;
+        int parentY = 0;
+        
+        //Do level-order traversal until x or y is found or queue is empty.
+        while(!queue.isEmpty() && !xFound && !yFound){
+            
+            int size = queue.size();
+			
+            //Traverse that level.
+            while(size-- > 0){
+                TreeNode node = queue.poll();
+				
+                //if x or y is found at left/right, save the parent and set the "found" flag to true.
+                //This flag will break the loop as soon as any one (x or y) is found. 
+				//we don't need to go deeper to find second if it isn't found at this level.
+				
+                if(node.left != null) {
+                    queue.offer(node.left);
+                    
+                    if(node.left.val == x){
+                        parentX = node.val;
+                        xFound = true;
+                    }
+                    if(node.left.val == y){
+                        parentY = node.val;
+                        yFound = true;
+                    }
+                    
+                }
+
+                if(node.right != null) {
+                    queue.offer(node.right);
+                    
+                    if(node.right.val == x){
+                        parentX = node.val;
+                        xFound = true;
+                    }
+                    if(node.right.val == y){
+                        parentY = node.val;
+                        yFound = true;
+                    }
+                }
+            }
+            
+        }
+        return xFound && yFound && parentX != parentY;
+    }   
+
+}
