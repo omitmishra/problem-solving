dc5f5b3ba6f733852b2f8a9b6b79cf2771582644
Ravi <49590835+omitmishra@users.noreply.github.com>
2022-01-01 10:14:54 +0530

    Time: 108 ms (52.80%), Space: 39.9 MB (12.82%) - LeetHub

---
+++ burst-balloons/burst-balloons.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxCoins(int[] arr) {
+         int[][] dp = new int[arr.length][arr.length];
+	  
+          for(int g = 0; g < dp[0].length; g++) {
+              for(int i = 0, j = g; j < dp[0].length; i++, j++) {
+                  int max = Integer.MIN_VALUE;
+                  for(int k = i; k <= j; k++) {
+                      int val = 0;
+                      if(k - 1 >= i)
+                          val += dp[i][k-1];
+                      if(k + 1 <= j)
+                          val += dp[k+1][j];
+                      int mul = arr[k];
+                      if(i - 1 >= 0)
+                          mul *= arr[i-1];
+                      if(j + 1 < arr.length)
+                          mul *= arr[j+1];
+                      val += mul;
+
+                      if(val > max)
+                          max = val;
+                  }
+                  dp[i][j] = max;
+              }
+          }
+          return dp[0][dp[0].length - 1];
+    }
+}
